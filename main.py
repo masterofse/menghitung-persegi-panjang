7ffd6b5223158f091cd27ce2e92461f891a02cab
masterofse <tinyboy1108@gmail.com>
2024-11-13 12:48:38 +0700

    initial commit with main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+#main py
+
+# meminta input panjang dan lebar dari pengguna
+panjang = float(input("masukan panjang pergi panjang:"))
+lebar = float(input("masukan lebar persegi panjang:"))
+
+#menghitung luas
+luas = panjang * lebar
+
+#menampilkan hasil
+print(f"luas persegi panjang adalah: {luas}")
